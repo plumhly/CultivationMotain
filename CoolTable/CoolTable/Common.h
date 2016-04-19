fb62e2db0b7ffa6f55008685d57638a4eb2e1cbc
libo <libo@MacBook-Air.local>
2016-04-19 21:11:17 +0800

    上传

--- CoolTable/CoolTable/Common.h
+++ CoolTable/CoolTable/Common.h
@@ -11,4 +11,8 @@
 
 @interface Common : NSObject
 void drawLinearGradient(CGContextRef context, CGRect rect, CGColorRef startColor, CGColorRef endColor);
+
+CGRect rectFor1PxStroke(CGRect rect);
+
+void draw1PxStroke(CGContextRef context, CGPoint startPoint, CGPoint endPoint, CGColorRef color);
 @end
--- CoolTable/CoolTable/Common.m
+++ CoolTable/CoolTable/Common.m
@@ -55,4 +55,20 @@ void drawLinearGradient(CGContextRef context, CGRect rect, CGColorRef startColor
     CGColorSpaceRelease(colorSpace);
     
 }
+
+CGRect rectFor1PxStroke(CGRect rect) {
+    return CGRectMake(rect.origin.x + 0.5, rect.origin.y + 0.5, rect.size.width - 1, rect.size.height - 1);
+}
+
+void draw1PxStroke(CGContextRef context, CGPoint startPoint, CGPoint endPoint, CGColorRef color) {
+    
+//    CGContextSaveGState(context);
+    CGContextSetLineCap(context, kCGLineCapSquare);
+    CGContextSetStrokeColorWithColor(context, color);
+    CGContextSetLineWidth(context, 1.0);
+    CGContextMoveToPoint(context, startPoint.x + 0.5, startPoint.y + 0.5);
+    CGContextAddLineToPoint(context, endPoint.x + 0.5, endPoint.y + 0.5);
+    CGContextStrokePath(context);
+//    CGContextRestoreGState(context);
+}
 @end
