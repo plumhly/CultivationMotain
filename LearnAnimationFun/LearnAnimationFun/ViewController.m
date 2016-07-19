d3f67a4c3c7f05573f4a769b25b1e5906a9062cd
libo <215747586@qq.com>
2016-07-19 18:30:06 +0800

    脸上3d

--- LearnAnimationFun/LearnAnimationFun/ViewController.m
+++ LearnAnimationFun/LearnAnimationFun/ViewController.m
@@ -12,6 +12,15 @@
 @property (weak, nonatomic) IBOutlet UIView *layerView;
 
 @property (weak, nonatomic) IBOutlet UIView *shadowView;
+
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+
+//test sublayerTransform
+
+@property (weak, nonatomic) IBOutlet UIImageView *leftView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *rightView;
+
 @property (strong, nonatomic) CALayer *blueLayer;
 @property (strong, nonatomic) CALayer *greenLayer;
 @end
@@ -26,7 +35,7 @@
 //    subLayer.backgroundColor = [UIColor redColor].CGColor;
 //    [self.layerView.layer addSublayer:subLayer];
     
-//    UIImage *image = [UIImage imageNamed:@"1.jpg"];
+//    UIImage *image = [UIImage imageNamed:@"Cone"];
 //    self.layerView.layer.contents = (__bridge id)image.CGImage;
 //    self.layerView.layer.contentsGravity = kCAGravityCenter;
 //    self.layerView.layer.shadowOpacity = 0.5;
@@ -41,7 +50,90 @@
 //    [self testLayerDelegate];
 //    [self testPositonAndArchorPoint];
 //    [self testContainPointAndHitTest];
-    [self testShadow];
+//    [self testShadow];
+//    [self testMask];
+//    [self testOpacity];
+//    [self testAffineTransiform];
+    [self testCATransform3D];
+}
+
+
+- (void)testCATransform3D {
+    _shadowView.layer.contents = (__bridge id)[UIImage imageNamed:@"Snowman"].CGImage;
+    
+    CATransform3D transform = CATransform3DMakeRotation(M_PI, 0, 1, 0);
+    
+    //透视
+//    CATransform3D transform = CATransform3DIdentity;
+//    transform.m34 = -1/500.0;
+    
+//    transform = CATransform3DRotate(transform, M_PI_4, 0, 1, 0);
+    
+    
+    _shadowView.layer.doubleSided = NO;//是否双面绘制
+    _shadowView.layer.transform = transform;
+    
+    // sublayerTransform
+    
+//    CATransform3D transform1 = CATransform3DMakeRotation(M_PI_4, 0, 1, 0);
+//    self.leftView.layer.transform = transform1;
+//    
+//    CATransform3D transform2 = CATransform3DMakeRotation(-M_PI_4, 0, 1, 0);
+//    self.rightView.layer.transform = transform2;
+    
+    //扁平化
+    
+}
+
+
+- (void)testAffineTransiform {
+    _shadowView.layer.contents = (__bridge id)[UIImage imageNamed:@"Snowman"].CGImage;
+    
+    //one way
+    CGAffineTransform transform = CGAffineTransformMakeRotation(M_PI / 6);
+    transform = CGAffineTransformTranslate(transform, 100, 0);
+    transform = CGAffineTransformScale(transform, 0.2, 0.2);
+    
+    
+    //other way
+//    CGAffineTransform transform = CGAffineTransformMakeRotation(M_PI / 6);
+//    transform = CGAffineTransformScale(transform, 0.2, 0.2);
+//    transform = CGAffineTransformConcat(transform, CGAffineTransformMakeTranslation(100, 0));
+    
+    _shadowView.layer.affineTransform = transform;
+    
+//    _shadowView.layer.transform = CATransform3DMakeRotation(M_PI_4, -1, -1, 5);
+}
+
+
+- (void)testOpacity {
+    _blueLayer = [CALayer layer];
+    _blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    _blueLayer.frame = CGRectMake(10, 10, 100, 100);
+    [_shadowView.layer addSublayer:_blueLayer];
+    
+    _greenLayer = [CALayer layer];
+    _greenLayer.backgroundColor = [UIColor greenColor].CGColor;
+    _greenLayer.frame = CGRectMake(0, 0, 50, 50);
+    [_blueLayer addSublayer:_greenLayer];
+    _blueLayer.opacity = 0.5;
+    _greenLayer.opacity = 0.5;
+    _blueLayer.shouldRasterize = YES;
+    _blueLayer.rasterizationScale = [UIScreen mainScreen].scale;
+    
+//    _shadowView.layer.opacity = 0.5;
+}
+
+- (void)testMask {
+    UIImage *image = [UIImage imageNamed:@"Igloo"];
+    self.shadowView.layer.contents = (__bridge id)image.CGImage;
+    
+    CALayer *maskLayer = [CALayer layer];
+    maskLayer.contents = (__bridge id)[UIImage imageNamed:@"Cone"].CGImage;
+    maskLayer.frame = self.shadowView.bounds;
+    
+    self.shadowView.layer.mask = maskLayer;
+    
 }
 
 - (void)testShadow {
@@ -94,6 +186,7 @@
 }
 
 - (void)testPositonAndArchorPoint {
+    /*
     UIView *testArchorPoint = [[UIView alloc]initWithFrame:CGRectMake(10, 10, 40, 50)];
     testArchorPoint.backgroundColor = [UIColor redColor];
     [self.layerView addSubview:testArchorPoint];
@@ -109,13 +202,24 @@
 //    self.layerView.layer.geometryFlipped = YES;
 //    self.view.transform = CGAffineTransformMakeTranslation(10, 10);
     testArchorPoint.layer.anchorPoint = CGPointMake(0, 0);
+    testArchorPoint.layer.frame = CGRectMake(20, 10, 40, 50);
 //    testArchorPoint.layer.geometryFlipped = YES;
     NSLog(@"After");
     NSLog(@"frame = %@",NSStringFromCGRect(testArchorPoint.layer.frame));
     NSLog(@"bounds = %@",NSStringFromCGRect(testArchorPoint.layer.bounds));
     NSLog(@"position = %@",NSStringFromCGPoint(testArchorPoint.layer.position));
     NSLog(@"archorPoint = %@",NSStringFromCGPoint(testArchorPoint.layer.anchorPoint));
+    */
     
+    CALayer *layer = [CALayer layer];
+    layer.backgroundColor = [UIColor redColor].CGColor;
+    layer.frame = CGRectMake(0, 0, 40, 50);
+    [self.shadowView.layer addSublayer:layer];
+    
+    NSLog(@"frame = %@",NSStringFromCGRect(layer.frame));
+    NSLog(@"bounds = %@",NSStringFromCGRect(layer.bounds));
+    NSLog(@"position = %@",NSStringFromCGPoint(layer.position));
+    NSLog(@"archorPoint = %@",NSStringFromCGPoint(layer.anchorPoint));
 }
 
 - (void)didReceiveMemoryWarning {
