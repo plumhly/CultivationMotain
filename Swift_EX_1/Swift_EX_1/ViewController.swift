d5ec63f01becd0a644df947b1e8f2635ebb1cc58
plum <215747586@qq.com>
2016-09-13 17:46:39 +0800

    swif练习

---
+++ Swift_EX_1/Swift_EX_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_EX_1
+//
+//  Created by plum on 16/9/13.
+//  Copyright © 2016年 plum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
