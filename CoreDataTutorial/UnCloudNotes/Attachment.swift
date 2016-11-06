dded76c0619105f20cdd65bf194004f77b868c84
plum <215747586@qq.com>
2016-11-06 18:44:53 +0800

    v2- v3

--- CoreDataTutorial/UnCloudNotes/Attachment.swift
+++ CoreDataTutorial/UnCloudNotes/Attachment.swift
@@ -3,21 +3,15 @@
 //  UnCloudNotes
 //
 //  Created by libo on 16/11/6.
-//  Copyright © 2016年 Ray Wenderlich. All rights reserved.
+//  Copyright © 2016年 plum. All rights reserved.
 //
 
 import Foundation
-
+import CoreData
 import UIKit
 
-import CoreData
-
 class Attachment: NSManagedObject {
-    
     @NSManaged var dateCreated: NSDate
-    
     @NSManaged var image: UIImage?
-    
     @NSManaged var note: Note
-    
 }
